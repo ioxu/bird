d3077cc933fc3d4ca810edf20265644e55b8c69d
benp <ben@ioxu.com>
2020-10-28 00:06:14 +1030

    mouse hides when exiting window

--- ui.gd
+++ ui.gd
@@ -7,10 +7,22 @@ extends Node2D
 
 onready var mouse_area : Area2D = $mouse_area
 
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	pass
 
+
 func _input(event):
    if event is InputEventMouseMotion:
 	   mouse_area.position = event.position
+
+
+func _notification(notification):
+	match notification:
+		NOTIFICATION_WM_MOUSE_EXIT:
+			mouse_area.visible = false
+			print('Mouse left window')
+		NOTIFICATION_WM_MOUSE_ENTER:
+			mouse_area.visible = true
+			print('Mouse entered window')
