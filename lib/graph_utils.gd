ce600f42b90b81049f3257d5894b44881a5e79cf
benp <ben@ioxu.com>
2020-11-21 22:24:54 +1030

    tidied dfs

--- lib/graph_utils.gd
+++ lib/graph_utils.gd
@@ -10,32 +10,27 @@ func parse_drawing(anchors: Array = []):
 	for a in anchors:
 		print("  anchor ", a.get_path())
 		var result  = DFS(a)
-	
+		#print("  result: ", result)
 	return {}
 
+
 func DFS(a):
-	print("  DFS")
-	print("    root node ", a.get_path())
 	var visited := []
 	var visited_nodes := []
 	var visited_segments := []
 	DFS_do(a, null, visited, visited_nodes, visited_segments)
-	
+	return {"visited": visited,
+		"visited_nodes": visited_nodes,
+		"visited_segments": visited_segments}
+
+
 func DFS_do(node, previous_segment, visited, visited_nodes, visited_segments):
-	# replace array searches (x in Array)
-	# with shorter lists
-	#print("  DFS_do")
 	for s in node.connected_segments:
-		#if not(s in visited_segments):
-		#print("is s = previous_segment: ", s, " ", previous_segment)
 		if s != previous_segment:
-			#print("  connected segment ", s, " ", s.get_path())
 			visited.append( s )
 			visited_segments.append( s )
 			for n in s.connected_nodes:
-				#if not(n in visited_nodes):
 				if n != node:
-					#print("  connected node ", n, " ", n.get_path())
 					visited.append( n )
 					visited_nodes.append(n)
 					n.update_label("order", visited_nodes.size() )
